054112a1123911194caacfc4b1e603da486010fc
PierreOF <marcos.pierre@upe.br>
2024-10-12 21:06:31 -0300

    feat : implementado SortByCompletionStatus para ordenar tasks por data de criação

---
+++ src/main/java/br/com/task/manager/strategy/SortByCreationDate.java
@@ -0,0 +1,14 @@
+package br.com.task.manager.strategy;
+
+import br.com.task.manager.model.Task;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SortByCreationDate implements TaskSortingStrategy {
+    @Override
+    public List<Task> sort(List<Task> tasks) {
+        tasks.sort(Comparator.comparing(Task::getCreationDate));
+        return tasks;
+    }
+}
