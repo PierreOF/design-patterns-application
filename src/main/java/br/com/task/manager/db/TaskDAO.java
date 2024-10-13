c10f77f0c7fd5cbe9bac2ae65a996cdc1c12cadb
Jeova BL <jeovab115@gmail.com>
2024-10-12 21:50:59 -0300

    feat<taskDAO>: criado a conexao e configuracao do taskDAO com o sqlite

---
+++ src/main/java/br/com/task/manager/db/TaskDAO.java
@@ -0,0 +1,52 @@
+package br.com.task.manager.db;
+
+import br.com.task.manager.model.Task;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskDAO {
+    private Connection connection;
+
+    public TaskDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void insert(Task task) {
+        String sql = "INSERT INTO tasks (title, description, status, user_id) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, task.getTitulo());
+            stmt.setString(2, task.getDescricao());
+            stmt.setString(3, task.getStatus());
+            stmt.setInt(4, task.getIdUsuario());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Task> getTasksByUsuario(int usuarioId) {
+        List<Task> tasks = new ArrayList<>();
+        String sql = "SELECT * FROM tasks WHERE user_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, usuarioId);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String titulo = rs.getString("title");
+                String descricao = rs.getString("description");
+                String status = rs.getString("status");
+                tasks.add(new Task(id, titulo, descricao, status, usuarioId));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return tasks;
+    }
+}
+
