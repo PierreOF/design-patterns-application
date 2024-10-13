622568d62bcd43f39e5dea8c61cdb2cecb2fd752
Jeova BL <jeovab115@gmail.com>
2024-10-13 15:13:17 -0300

    feat<TaskProxyDAOInterface>: interface para implementação do padrao proxy

---
+++ src/main/java/br/com/task/manager/db/proxy/TaskProxyDAOInterface.java
@@ -0,0 +1,14 @@
+package br.com.task.manager.db.proxy;
+
+import br.com.task.manager.model.Task;
+
+import java.util.List;
+
+public interface TaskProxyDAOInterface {
+    void insertTask(Task task);
+    void updateTask(Task task);
+    void deleteTaskById(int id);
+    List<Task> getTasksByUserId(int userId);
+    Task getTaskById(int taskId);
+    void deleteTasksByUsuarioId(int userId);
+}
