7d1b7d0a692716c64b2705ec8a000552c60c2540
Arthur <arthur.sampaio@upe.br>
2024-10-13 18:10:03 -0300

    feat(UsuarioTaskObserver): criando logica de notificação do usuario com observer

---
+++ src/main/java/br/com/task/manager/observer/UsuarioTaskObserver.java
@@ -0,0 +1,21 @@
+package br.com.task.manager.observer;
+
+import br.com.task.manager.email.EmailService;
+import br.com.task.manager.model.Usuario;
+
+public class UsuarioTaskObserver implements TaskObserver {
+
+    private final Usuario usuario;
+    private final EmailService emailService;
+
+    public UsuarioTaskObserver(Usuario usuario, EmailService emailService) {
+        this.usuario = usuario;
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void update(String taskDetails) {
+        System.out.println("Notificando usuário " + usuario.getNome() + " sobre task: " + taskDetails);
+        emailService.sendEmail(usuario.getEmail(), "Atualização de Tarefa", taskDetails);
+    }
+}
