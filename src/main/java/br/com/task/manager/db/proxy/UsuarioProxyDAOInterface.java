313326b5e4905bd30fe97e6a7080f7e12a1a5a3a
Jeova BL <jeovab115@gmail.com>
2024-10-13 15:13:27 -0300

    feat<UsuarioProxyDAOInterface>: interface para implementação do padrao proxy

---
+++ src/main/java/br/com/task/manager/db/proxy/UsuarioProxyDAOInterface.java
@@ -0,0 +1,14 @@
+package br.com.task.manager.db.proxy;
+
+import br.com.task.manager.model.Usuario;
+
+import java.util.List;
+
+public interface UsuarioProxyDAOInterface {
+    Usuario userLogin(String email, String senha);
+    void insertUser(Usuario usuario);
+    void updateUser(Usuario usuario);
+    void deleteUser(int id);
+    List<Usuario> getAllUsuarios();
+    Usuario getUsuarioById(int id);
+}
