bac31692d94e72a45bf212ac4617640e8c3af2dc
Jeova BL <jeovab115@gmail.com>
2024-10-12 21:50:17 -0300

    feat<sqlCon>: criado a conexao e configuracao do sqlite

---
+++ src/main/java/br/com/task/manager/db/SQLiteConnection.java
@@ -0,0 +1,59 @@
+package br.com.task.manager.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteConnection {
+
+    private static final String DATABASE_URL = "jdbc:sqlite:task-manager.db";
+    private static Connection connection;
+
+    public static Connection connect() {
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    public static void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void createTables() {
+        String createUsersTable = "CREATE TABLE IF NOT EXISTS users ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "name TEXT NOT NULL,"
+                + "email TEXT NOT NULL,"
+                + "password TEXT NOT NULL"
+                + ");";
+
+        String createTasksTable = "CREATE TABLE IF NOT EXISTS tasks ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "title TEXT NOT NULL,"
+                + "description TEXT NOT NULL,"
+                + "status TEXT NOT NULL,"
+                + "user_id INTEGER NOT NULL,"
+                + "FOREIGN KEY(user_id) REFERENCES users(id)"
+                + ");";
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(createUsersTable);
+            stmt.execute(createTasksTable);
+            System.out.println("Tabelas criadas ou já existem.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+}
