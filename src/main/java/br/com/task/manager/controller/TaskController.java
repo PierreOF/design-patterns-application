b5cc900e7cb258ec93504e90a61959d975b0c34c
Jeova BL <jeovab115@gmail.com>
2024-10-13 15:10:38 -0300

    fix: deletado o taskController para implementar correcoes no proxy

--- src/main/java/br/com/task/manager/controller/TaskController.java
+++ src/main/java/br/com/task/manager/controller/TaskController.java
@@ -1,16 +1,5 @@
 package br.com.task.manager.controller;
 
-import br.com.task.manager.model.Task;
-import br.com.task.manager.proxy.UsuarioProxy;
 
 public class TaskController {
-    private UsuarioProxy database;
-
-    public TaskController(UsuarioProxy database) {
-        this.database = database;
-    }
-
-    public void addTask(Task task) {
-        database.addTask(task);
-    }
 }
