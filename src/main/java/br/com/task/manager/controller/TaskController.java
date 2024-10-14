10bef74a07bfd7e1a46a2b89f7af21785a96c6e9
Jeova BL <jeovab115@gmail.com>
2024-10-14 18:02:29 -0300

    fix<TaskController>: implementado a validacao

--- src/main/java/br/com/task/manager/controller/TaskController.java
+++ src/main/java/br/com/task/manager/controller/TaskController.java
@@ -1,6 +1,9 @@
 package br.com.task.manager.controller;
 
 
+import br.com.task.manager.controller.validation.ResultValidationEnum;
+import br.com.task.manager.controller.validation.TaskValidation;
+import br.com.task.manager.controller.validation.interfaces.TaskInterfaceValidation;
 import br.com.task.manager.db.proxy.TaskProxyDAOInterface;
 import br.com.task.manager.model.Task;
 import br.com.task.manager.observer.TaskNotifier;
@@ -10,22 +13,34 @@ import java.util.List;
 
 public class TaskController {
 
+    private final TaskInterfaceValidation taskValidation;
     private final TaskProxyDAOInterface database;
     private final TaskNotifier taskNotifier;
 
     public TaskController(TaskProxyDAOInterface database, TaskNotifier taskNotifier) {
         this.database = database;
         this.taskNotifier = taskNotifier;
+        this.taskValidation = new TaskValidation();
     }
 
-    public void addTask(Task task) {
+    public ResultValidationEnum addTask(Task task) {
+        ResultValidationEnum result = taskValidation.validateTask(task);
+        if (result == ResultValidationEnum.REJECTED) {
+            return result;
+        }
         database.insertTask(task);
         taskNotifier.notifyObservers("Nova task criada: " + task.getTitulo());
+        return result;
     }
 
-    public void updateTask(Task task) {
+    public ResultValidationEnum updateTask(Task task) {
+        ResultValidationEnum result = taskValidation.validateTask(task);
+        if (result == ResultValidationEnum.REJECTED) {
+            return result;
+        }
         database.updateTask(task);
         taskNotifier.notifyObservers("Task atualizada: " + task.getTitulo());
+        return result;
     }
 
     public void deleteTask(int taskId) {
