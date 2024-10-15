59ffc0112f990c62fd1595c7387bce5b3da7ba92
Arthur <arthur.sampaio@upe.br>
2024-10-15 08:49:23 -0300

    feat(UsuarioController): adicionando observer de envio de email

--- src/main/java/br/com/task/manager/controller/UsuarioController.java
+++ src/main/java/br/com/task/manager/controller/UsuarioController.java
@@ -5,7 +5,8 @@ import br.com.task.manager.controller.validation.UserValidation;
 import br.com.task.manager.controller.validation.interfaces.UserInterfaceValidation;
 import br.com.task.manager.db.proxy.UsuarioProxy;
 import br.com.task.manager.db.proxy.UsuarioProxyDAOInterface;
-import br.com.task.manager.email.EmailService;
+import br.com.task.manager.observer.EmailObserver;
+import br.com.task.manager.observer.email.EmailService;
 import br.com.task.manager.model.Usuario;
 import br.com.task.manager.observer.TaskNotifier;
 import br.com.task.manager.observer.UsuarioTaskObserver;
@@ -57,8 +58,8 @@ public class UsuarioController {
         }
 
         usuarioProxy.insertUser(novoUsuario);
-        emailService.sendEmail(email, "Conta criada com sucesso", "Parabéns sua conta foi criado com sucesso!");
-        System.out.println("Email enviado com sucesso!");
+        taskNotifier.addObserver(new EmailObserver(emailService, novoUsuario.getEmail()));
+        taskNotifier.notifyObservers("Sucesso ao registrar");
         return ResultValidationEnum.APPROVED;
     }
 
