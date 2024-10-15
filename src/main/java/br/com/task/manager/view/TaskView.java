d15c1aabca95a84e483ae97e4f6749a0338c70a6
Jeova BL <jeovab115@gmail.com>
2024-10-14 21:07:35 -0300

    feat<TaskView>: Visualização do update e correção de erros nas seleçoes

--- src/main/java/br/com/task/manager/view/TaskView.java
+++ src/main/java/br/com/task/manager/view/TaskView.java
@@ -1,7 +1,9 @@
 package br.com.task.manager.view;
 
 import br.com.task.manager.controller.TaskController;
+import br.com.task.manager.controller.validation.ResultValidationEnum;
 import br.com.task.manager.model.Enum.TaskPriorityEnum;
+import br.com.task.manager.model.Enum.TaskStatusEnum;
 import br.com.task.manager.model.Task;
 import br.com.task.manager.strategy.SortByCompletionStatus;
 import br.com.task.manager.strategy.SortByCreationDate;
@@ -23,17 +25,22 @@ public class TaskView {
     }
 
     public void menu() {
-        int opcao;
+        Integer opcao = 0;
         do {
             System.out.println("\n");
             System.out.println("---- Menu de Task ----");
             System.out.println("1. Mostrar as tasks");
             System.out.println("2. Criar uma task");
-            System.out.println("3. Insira um id para deletar uma task");
-            System.out.println("4. Sair");
+            System.out.println("3. Deletar uma task");
+            System.out.println("4. Atualizar uma task");
+            System.out.println("5. Sair");
             System.out.print("Escolha uma opção: ");
-            opcao = Integer.parseInt(scanner.nextLine());
-
+            try {
+                opcao = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Opção inválida.\n");
+                continue;
+            }
             switch (opcao) {
                 case 1:
                     displayTasks();
@@ -45,17 +52,79 @@ public class TaskView {
                     deleteTask();
                     break;
                 case 4:
+                    atualizarTask();
+                    break;
+                case 5:
                     System.out.println("Saindo...");
-                    taskController.clearCacheByUserId(userId);
+                    break;
                 default:
                     System.out.println("Opção inválida.");
             }
-        } while (opcao != 4);
+        } while (opcao != 5);
+    }
+
+    private void atualizarTask() {
+        int taskId = getTaskIdForUse();
+        Task task = taskController.getTaskById(taskId);
+        if (task == null) {
+            System.out.println("Tarefa não encontrada.");
+            return;
+        }
+        showTask(task);
+        task = setNewDataToTask(task);
+        ResultValidationEnum resultValidation = taskController.updateTask(task);
+        if (resultValidation == ResultValidationEnum.REJECTED) {
+            System.out.println("Erro ao atualizar a tarefa.");
+        } else {
+            System.out.println("Tarefa atualizada com sucesso!");
+        }
+    }
+
+    private Task setNewDataToTask(Task task) {
+        System.out.println("---- Digite os novos valores ----");
+        System.out.print("Titulo: ");
+        String title = scanner.nextLine();
+        System.out.print("Descrição: ");
+        String description = scanner.nextLine();
+        System.out.print("Status (Pendente, Cancelada, Concluida): ");
+        String status = scanner.nextLine();
+        System.out.print("Prioridade (Urgente, Importante, Normal, Baixa): ");
+        String priority = scanner.nextLine();
+        return new Task(title, description, status, task.getIdUsuario(), priority, task.getCreationDate());
+    }
+
+    private void showTask(Task task) {
+        System.out.println("---- Detalhes da Task ----");
+        System.out.println("ID: " + task.getId());
+        System.out.println("Titulo: " + task.getTitulo());
+        System.out.println("Descrição: " + task.getDescricao());
+        System.out.println("Status: " + task.getStatus());
+        System.out.println("Prioridade: " + task.getPriority());
+        System.out.println("Data de criação: " + task.getCreationDate());
+        System.out.println("--------------------------");
     }
 
     private void displayTasks() {
-        System.out.print("Escolha a estratégia de ordenação (1: PRIORIDADE, 2: STATUS, 3: DATA): ");
-        int sortOption = Integer.parseInt(scanner.nextLine());
+        int sortOption = 0;
+        do {
+            try {
+                System.out.println("---- Opções de ordenação ----");
+                System.out.println("1. Ordenar por prioridade");
+                System.out.println("2. Ordenar por status de conclusão");
+                System.out.println("3. Ordenar por data de criação");
+                System.out.println("4. Voltar");
+                System.out.print("Escolha uma opção: ");
+                sortOption = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Opção inválida.\n");
+                continue;
+            }
+        } while (sortOption < 1 || sortOption > 4);
+
+        if (sortOption == 4) {
+            return;
+        }
+
         TaskSortingStrategy sortingStrategy = getSortingStrategy(sortOption);
 
         List<Task> tasks = taskController.getAllTasks(userId, sortingStrategy);
@@ -77,7 +146,7 @@ public class TaskView {
     }
 
     private void deleteTask() {
-        int taskId = getTaskIdForDeletion();
+        int taskId = getTaskIdForUse();
         taskController.deleteTask(taskId);
         System.out.println("Tarefa deletada com sucesso!");
     }
@@ -92,8 +161,8 @@ public class TaskView {
         return new Task(title, description, userId, TaskPriorityEnum.fromDescription(priority));
     }
 
-    private int getTaskIdForDeletion() {
-        System.out.print("Digite o ID da tarefa para deletar: ");
+    private int getTaskIdForUse() {
+        System.out.print("Digite o ID da task: ");
         return Integer.parseInt(scanner.nextLine());
     }
 
